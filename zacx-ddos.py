a8877076baf36939a51e9481a01e700dbdb8e912
OZKON24 <za9474@yahoo.com>
2024-11-26 02:27:15 +0700

    Update zacx-ddos.py

--- zacx-ddos.py
+++ zacx-ddos.py
@@ -31,15 +31,15 @@ print("")
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
